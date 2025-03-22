a37086b14a9e1807d35ba1d62b943e9c55856843
asatormk6 <asatormk6@gmail.com>
2025-03-23 04:38:59 +1100

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    output: 'static',
-    build: {
-        outDir: 'docs',
-    },
+  output: 'static',
+  base: '/freesexchatbot-site/', // <-- this must match your GitHub repo name
+  build: {
+    outDir: 'docs',
+  },
 });
